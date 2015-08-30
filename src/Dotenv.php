29404355f4d56b34744124f351c3e3cba82f51cd
Petr Cibulka <cibulka.me@gmail.com>
2015-08-30 16:34:11 +0200

    Inital commit.

---
+++ src/Dotenv.php
@@ -0,0 +1,5 @@
+<?php
+	
+namespace Cibulka\Notenv;
+
+class Dotenv extends Dotenv\Dotenv {}
---
+++ src/Loader.php
@@ -0,0 +1,63 @@
+<?php
+	
+namespace Cibulka\Notenv;
+
+class Loader extends Dotenv\Loader {
+	
+	protected $notenv_data;
+	
+	protected $notenv_callback;
+	
+	public function getEnvironmentVariable($name) {
+		
+		if (empty($_ENV)) {
+			throw new \Exception('Empty $_ENV variable, switch by something else.');
+		}
+		
+		switch (true) {
+			case array_key_exists($name, $_ENV):
+				return $_ENV[$name];
+			break;
+			case array_key_exists($name, $_SERVER):
+				return $_SERVER[$name];
+			break;
+			case array_key_exists($name, this->notenv_data):
+				return $this->notenv_data[$name];
+			break;
+		}
+		
+	}
+	
+	public function setEnvironmentVariable($name, $data) {
+		
+        list($name, $value) = $this->normaliseEnvironmentVariable($name, $value);
+
+        // Don't overwrite existing environment variables if we're immutable
+        // Ruby's dotenv does this with `ENV[key] ||= value`.
+        if ($this->immutable === true && !is_null($this->getEnvironmentVariable($name))) {
+            return;
+        }
+        
+        if (!empty($this->notenv_callback)) {
+	        call_user_func_array($this->notenv_callback, func_get_args());
+        }
+        
+        $this->notenv_data[$name] = $data;
+		
+	}
+	
+	public function clearEnvironmentVariable($name) {
+		
+		parent::cleanEnvironmentVariable($name);
+		
+		if (!$this->immutable) {
+			unset($this->notenv_data[$name]);
+		}
+		
+	}
+	
+	public function setVariableSetter($callback) {
+		$this->notenv_callback = $callback;
+	}
+	
+}
